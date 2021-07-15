ccbe65d307505805b4603ed76bf58daaa6653585
Jorge Lameiro <jlameiro@farelogix.com>
2021-07-15 15:54:34 -0400

    Continue refactoring

--- src/App.js
+++ src/App.js
@@ -4,33 +4,52 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+
 import Header from './components/Header';
 import Welcome from './components/Welcome';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#333'
+    },
+    secondary: {
+      main: '#e0e0e0'
+    }
+  }
+});
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Router>
         <Header />
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/portfolio">
-            <Portfolio />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/">
-            <Welcome />
-          </Route>
-        </Switch>
+        <Container>
+          <Typography component="div" style={{ height: '90vh' }}>
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/portfolio">
+                <Portfolio />
+              </Route>
+              <Route path="/contact">
+                <Contact />
+              </Route>
+              <Route path="/">
+                <Welcome />
+              </Route>
+            </Switch>
+          </Typography>
+        </Container>
       </Router>
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom";
-import { makeStyles, ThemeProvider, createTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import { purple, blueGrey } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,40 +19,27 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: purple[500]
-    },
-    secondary: {
-      main: blueGrey[500]
-    }
-  }
-});
-
 const Header = () => {
   const classes = useStyles();
 
   return (
-    <ThemeProvider theme={theme}>
-      <div className={classes.root}>
-        <AppBar position="static" color="primary">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              My Portfolio
-            </Typography>
+    <div className={classes.root}>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            My Portfolio
+          </Typography>
 
-            <Link to="/"><Button className={classes.menuButton} variant="contained" color="secondary">Welcome</Button></Link>
-            <Link to="/about"><Button className={classes.menuButton} variant="contained" color="secondary">About</Button></Link>
-            <Link to="/portfolio"><Button className={classes.menuButton} variant="contained" color="secondary">Portfolio</Button></Link>
-            <Link to="/contact"><Button className={classes.menuButton} variant="contained" color="secondary">Contact</Button></Link>
-          </Toolbar>
-        </AppBar>
-      </div>
-    </ThemeProvider>
+          <Link to="/"><Button className={classes.menuButton} variant="contained" color="secondary">Welcome</Button></Link>
+          <Link to="/about"><Button className={classes.menuButton} variant="contained" color="secondary">About</Button></Link>
+          <Link to="/portfolio"><Button className={classes.menuButton} variant="contained" color="secondary">Portfolio</Button></Link>
+          <Link to="/contact"><Button className={classes.menuButton} variant="contained" color="secondary">Contact</Button></Link>
+        </Toolbar>
+      </AppBar>
+    </div>
   )
 }
 
