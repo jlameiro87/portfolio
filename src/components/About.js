6cce2f134f9723704d2b2430f06729835b276c96
JorG <jlameiro87@gmail.com>
2021-09-15 18:25:46 -0400

    Added portfolio items

--- src/components/About.js
+++ src/components/About.js
@@ -19,10 +19,11 @@ import TimelineContent from '@material-ui/lab/TimelineContent';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: '80%',
+    width: '80%',
   },
   avatar: {
     backgroundColor: red[500],
@@ -38,19 +39,23 @@ const About = () => {
   const certifications = [
     {
       id: 1,
-      text: 'B.Sc. Software Engineer.'
+      text: 'B.Sc. Software Engineer.',
+      url: null
     },
     {
       id: 2,
-      text: 'CS50\'s Introduction to Computer Science.'
+      text: 'CS50\'s Introduction to Computer Science.',
+      url: 'https://certificates.cs50.io/1918fa46-f205-4600-973c-0f35d12c9521.png?size=letter'
     },
     {
       id: 3,
-      text: 'CS50\'s Web Programming with Python and JavaScript.'
+      text: 'CS50\'s Web Programming with Python and JavaScript.',
+      url: 'https://certificates.cs50.io/343b8991-fcbe-4ebb-83e5-220d1640668c.png?size=letter'
     },
     {
       id: 4,
-      text: 'FreeCodeCamp\'s JavaScript Algorithms and Data Structures.'
+      text: 'FreeCodeCamp\'s JavaScript Algorithms and Data Structures.',
+      url: 'https://www.freecodecamp.org/certification/jlameiro/javascript-algorithms-and-data-structures'
     }
   ];
 
@@ -122,9 +127,8 @@ const About = () => {
               {
                 certifications.map(item =>
                   <ListItem key={item.id}>
-                    <ListItemText
-                      primary={item.text}
-                    />
+                    {item.url && <Link href={item.url} target="_blank">{item.text}</Link>}
+                    {!item.url && <ListItemText primary={item.text} />}
                   </ListItem>
                 )
               }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,59 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '80%',
+  },
+  divRoot: {
+    display: 'flex',
+    justifyContent: 'center'
+  }
+}));
+
 const Portfolio = () => {
+  const classes = useStyles();
+  const websites = [
+    {
+      id: 1,
+      text: 'My Weather',
+      url: 'https://jlameiro-myweather.netlify.app/'
+    },
+    {
+      id: 2,
+      text: 'Rock - Pappers - Scissors Game',
+      url: 'https://pedantic-neumann-171bbe.netlify.app/'
+    },
+    {
+      id: 3,
+      text: 'Task Tracker',
+      url: 'https://focused-johnson-53f5f6.netlify.app/'
+    }
+  ]
+
   return (
-    <div>
-      Portfolio
+    <div className={classes.divRoot}>
+      <Card className={classes.root}>
+        <CardContent>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+            <Typography variant="h6">Projects</Typography>
+            <List dense={true}>
+              {
+                websites.map(item =>
+                  <ListItem key={item.id}>
+                    <Link href={item.url} target="_blank">{item.text}</Link>
+                  </ListItem>
+                )
+              }
+            </List>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   )
 }
